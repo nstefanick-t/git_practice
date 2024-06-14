d8884052193a85a7baec3533dd047961d16784ae
Nathan Stefanick <nstefanick@TECHNOMICS.NET>
2024-06-14 10:29:21 -0400

    commit changes made from feature_local merged to feature_origin

--- file1.R
+++ file1.R
@@ -3,3 +3,7 @@
 # changes made to file1
 
 # change and commit to feature origin
+
+# making and saving uncommitted changes with the intention of making a local split feature branch
+
+# making changes on feature local
